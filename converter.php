cb7a491d596142f70a6c43f922f49e19010b8ceb
MattiaBLX <36816061+MattiaBLX@users.noreply.github.com>
2018-08-30 15:37:51 +0200

    Feature.

--- converter.php
+++ converter.php
@@ -1,10 +1,11 @@
 <?php
 $directory = '';
+$dest = '':
 $canzoni = glob($directory.'*mp3');
 foreach($canzoni as $song){
     $song = str_replace($directory, '', $song);
     $output = str_replace('.mp3', '', $song);
     echo "Converting ".$song." to $output.raw".PHP_EOL.PHP_EOL;
-    @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$directory$output.raw'");
+    @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.raw'");
     @exec("sudo rm '$directory$song'");
 }
