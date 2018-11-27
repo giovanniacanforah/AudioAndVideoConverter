f40921c145aefa8082e0b0ac86b05667051af30a
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2018-11-27 18:03:35 +0100

    bug fix

--- converter.php
+++ converter.php
@@ -6,7 +6,7 @@ $to = readline("In quale estensione devo convertire?");
 $canzoni = glob($directory.'.'.$from);
 foreach($canzoni as $song){
     $song = str_replace($directory, '', $song);
-    $output = str_replace('.mp3', '', $song);
+    $output = str_replace('.'.$to, '', $song);
     echo "Converting ".$song." to $output.$to".PHP_EOL.PHP_EOL;
     @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.$to'");
     @exec("sudo rm '$directory$song'");
