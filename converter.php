4ce032a10a88f3a217df49fd8f11196f4790acce
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2018-11-27 18:00:40 +0100

    Update converter.php

--- converter.php
+++ converter.php
@@ -7,7 +7,7 @@ $canzoni = glob($directory.'.'.$from);
 foreach($canzoni as $song){
     $song = str_replace($directory, '', $song);
     $output = str_replace('.mp3', '', $song);
-    echo "Converting ".$song." to $output.raw".PHP_EOL.PHP_EOL;
+    echo "Converting ".$song." to $output.$to".PHP_EOL.PHP_EOL;
     @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.$to'");
     @exec("sudo rm '$directory$song'");
 }
