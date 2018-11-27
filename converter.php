76d1c37155492f867a2d7e6a9c424f0da7c84151
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2018-11-27 18:00:06 +0100

    Ora si possono selezionare i formati

--- converter.php
+++ converter.php
@@ -1,11 +1,13 @@
 <?php
 $directory = '';
 $dest = '';
-$canzoni = glob($directory.'*mp3');
+$from = readline("Da quale estensione devo convertire?"); 
+$to = readline("In quale estensione devo convertire?"); 
+$canzoni = glob($directory.'.'.$from);
 foreach($canzoni as $song){
     $song = str_replace($directory, '', $song);
     $output = str_replace('.mp3', '', $song);
     echo "Converting ".$song." to $output.raw".PHP_EOL.PHP_EOL;
-    @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.raw'");
+    @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.$to'");
     @exec("sudo rm '$directory$song'");
 }
