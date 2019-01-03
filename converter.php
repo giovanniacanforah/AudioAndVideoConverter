0c054cf1f4fcfdc4dd15db32056a8f827808fc8d
giovanniacanforah <schizzapple@gmail.com>
2019-01-03 20:03:19 +0100

    Update converter.php
    Aggiunta multi-lang e chiusura automatica dello script in caso di lingua o estensione non valida.

--- converter.php
+++ converter.php
@@ -1,13 +1,17 @@
 <?php
+$lang = readline("What language should I use? [it/en]");
+if(empty($lang) or !in_array($lang, ['en', 'it'])) die("Error.");
 $directory = '';
 $dest = '';
-$from = readline("Da quale estensione devo convertire?"); 
-$to = readline("In quale estensione devo convertire?"); 
+$from = PHP_EOL.readline(['it' => "Da quale estensione devo convertire?", 'en' => "What extension should I convert?"][$lang]); 
+$to = PHP_EOL.readline(['it' => "In quale estensione devo convertire?", 'en' => "In what extension do I have to convert?"][$lang]); 
+if(empty($from) or empty($to)) die(['it' => 'Errore.', 'en' => 'Error.'][$lang]);
 $canzoni = glob($directory.'.*'.$from);
 foreach($canzoni as $song){
     $song = str_replace($directory, '', $song);
     $output = str_replace('.'.$to, '', $song);
-    echo "Converting ".$song." to $output.$to".PHP_EOL.PHP_EOL;
+    echo PHP_EOL.['en' => "Converting ".$song." to $output.$to", 'it' => "Conversione ".$song." in $output.$to"][$lang].PHP_EOL.PHP_EOL;
     @exec("ffmpeg -i '$directory$song' -f s16le -ac 1 -ar 48000 -acodec pcm_s16le '$dest$output.$to'");
     @exec("sudo rm '$directory$song'");
 }
+echo PHP_EOL.['en' => 'Finish!', 'it' => 'Finito!'][$lang];
